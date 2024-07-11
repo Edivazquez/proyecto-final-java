d4fd1f2dabe249ef51641b955273e2ba51ba6d19
Edivazquez <vazquez_edgar13@hotmail.com>
2024-07-11 11:09:25 -0600

    primer proyecto

---
+++ app/src/main/java/com/example/myapplication/Nuevo proyecto.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+    fun main() {
+       val diaSemana = 5
+
+        when (diaSemana) {
+
+            1 -> println("lunes")
+            2 -> println("Martes")
+            3 -> println("Miercoles")
+            4 -> println("Jueves")
+            5 -> println("Viernes")
+            else -> println("fin de Semana")
+
+        }
+    }
---
+++ app/src/main/java/com/example/myapplication/practica 2.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+fun main (){
+
+    val javon= 50.50
+    val huevo = 10.00
+    val salchichas = 30.55
+    val queso =1.00
+    val total = (3*javon) + (10*huevo) + (20*salchichas) + (25*queso)
+
+    println("el total es $total")
+
+}
+
